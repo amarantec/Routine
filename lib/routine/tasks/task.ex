97b7c503000f91a97dae93903a3784535ca2b365
amarantec <marcoscarvalhop@outlook.com>
2025-10-14 20:02:37 -0300

    Task module from Routines

---
+++ lib/routine/tasks/task.ex
@@ -0,0 +1,24 @@
+defmodule Routine.Tasks.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "tasks" do
+    field :name, :string
+    field :description, :string
+    field :done, :boolean, default: false
+    field :redline, :naive_datetime_usec
+    field :user_id, :binary_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(task, attrs, user_scope) do
+    task
+    |> cast(attrs, [:name, :description, :done, :redline])
+    |> validate_required([:name, :description, :done, :redline])
+    |> put_change(:user_id, user_scope.user.id)
+  end
+end
---
+++ lib/routine_web/live/task_live/index.ex
@@ -0,0 +1,76 @@
+defmodule RoutineWeb.TaskLive.Index do
+  use RoutineWeb, :live_view
+
+  alias Routine.Tasks
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash} current_scope={@current_scope}>
+      <.header>
+        Listing Tasks
+        <:actions>
+          <.button variant="primary" navigate={~p"/tasks/new"}>
+            <.icon name="hero-plus" /> New Task
+          </.button>
+        </:actions>
+      </.header>
+
+      <.table
+        id="tasks"
+        rows={@streams.tasks}
+        row_click={fn {_id, task} -> JS.navigate(~p"/tasks/#{task}") end}
+      >
+        <:col :let={{_id, task}} label="Name">{task.name}</:col>
+        <:col :let={{_id, task}} label="Description">{task.description}</:col>
+        <:col :let={{_id, task}} label="Done">{task.done}</:col>
+        <:col :let={{_id, task}} label="Redline"> <%= Calendar.strftime(task.redline, "%H:%M - %d/%m/%Y") %> </:col>
+        <:action :let={{_id, task}}>
+          <div class="sr-only">
+            <.link navigate={~p"/tasks/#{task}"}>Show</.link>
+          </div>
+          <.link navigate={~p"/tasks/#{task}/edit"}>Edit</.link>
+        </:action>
+        <:action :let={{id, task}}>
+          <.link
+            phx-click={JS.push("delete", value: %{id: task.id}) |> hide("##{id}")}
+            data-confirm="Are you sure?"
+          >
+            Delete
+          </.link>
+        </:action>
+      </.table>
+    </Layouts.app>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Tasks.subscribe_tasks(socket.assigns.current_scope)
+    end
+
+    {:ok,
+     socket
+     |> assign(:page_title, "Listing Tasks")
+     |> stream(:tasks, list_tasks(socket.assigns.current_scope))}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    task = Tasks.get_task!(socket.assigns.current_scope, id)
+    {:ok, _} = Tasks.delete_task(socket.assigns.current_scope, task)
+
+    {:noreply, stream_delete(socket, :tasks, task)}
+  end
+
+  @impl true
+  def handle_info({type, %Routine.Tasks.Task{}}, socket)
+      when type in [:created, :updated, :deleted] do
+    {:noreply, stream(socket, :tasks, list_tasks(socket.assigns.current_scope), reset: true)}
+  end
+
+  defp list_tasks(current_scope) do
+    Tasks.list_tasks(current_scope)
+  end
+end
