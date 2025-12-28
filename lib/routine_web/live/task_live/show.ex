26247582221549858152e3ec287b07e632cb795c
amarantec <marcoscarvalhop@outlook.com>
2025-12-27 22:17:27 -0300

    First version of review feature

--- lib/routine_web/live/task_live/show.ex
+++ lib/routine_web/live/task_live/show.ex
@@ -31,6 +31,15 @@ defmodule RoutineWeb.TaskLive.Show do
         <:item title="Done">{@task.done}</:item>
         <:item title="Redline">{Calendar.strftime(@task.redline, "%H:%M - %d/%m/%Y")}</:item>
       </.list>
+	
+      <%= if NaiveDateTime.compare(@task.redline, NaiveDateTime.local_now()) == :lt do %>
+        <.form for={@form} id="task-review-form" phx-change="validate_review" phx-submit="save_review">
+        <.input field={@form[:review]} type="textarea" label="Review"/>
+	<div class="mt-4">
+	  <.button type="submit" variant="primary"> Save review</.button>
+        </div>
+        </.form>
+      <% end %>
     </Layouts.app>
     """
   end
@@ -40,11 +49,12 @@ defmodule RoutineWeb.TaskLive.Show do
     if connected?(socket) do
       Tasks.subscribe_tasks(socket.assigns.current_scope)
     end
-
+    task = Tasks.get_task!(socket.assigns.current_scope, id)
     {:ok,
      socket
      |> assign(:page_title, "Show Task")
-     |> assign(:task, Tasks.get_task!(socket.assigns.current_scope, id))}
+     |> assign(:task, task)
+     |> apply_action(socket.assigns.live_action, task)}
   end
 
   @impl true
@@ -79,4 +89,60 @@ defmodule RoutineWeb.TaskLive.Show do
 
     {:noreply, assign(socket, deleted_task: task)}
   end
+
+  defp apply_action(socket, :show, task) do
+    socket
+    |> assign(:page_title, "Show Task")
+    |> assign(:task, task)
+    |> assign(:form, to_form(Tasks.change_task(socket.assigns.current_scope, task)))
+  end
+    
+  defp apply_action(socket, :edit, task) do
+    socket
+    |> assign(:page_title, "Submit a Review")
+    |> assign(:task, task)
+    |> assign(:form, to_form(Tasks.change_task(socket.assigns.current_scope, task)))
+  end
+
+  def handle_event("validate_review", %{"task" => review_params}, socket) do
+    changeset = review_changeset(socket.assigns.task, review_params)
+    {:noreply, assign(socket, form: to_form(changeset))}
+  end
+  
+  def handle_event("save_review", %{"task" => review_params}, socket) do
+    case Tasks.update_task(socket.assigns.current_scope, socket.assigns.task, review_params) do
+      {:ok, task} ->
+	{:noreply,
+	 socket
+	 |> put_flash(:info, "Review submitted successfully")
+	 |> assign(:task, task)
+	 |> assign(:form, to_form(Tasks.change_task(socket.assigns.current_scope, task)))}
+      {:error, %Ecto.Changeset{} = changeset} ->
+	{:noreply, assign(socket, form: to_form(changeset))}
+    end
+  end
+  
+  defp review_task(socket, :edit, task_params) do
+    case Tasks.update_task(socket.assigns.current_scope, socket.assigns.task, task_params) do
+      {:ok, task} ->
+	{:noreply,
+	 socket
+	 |> put_flash(:info, "Review submited successfully")
+	 |> push_navigate(
+	   to: return_path(socket.assigns.current_scope, socket.assigns.return_to, task)
+	 )}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+	{:noreply, assign(socket, form: to_form(changeset))}
+    end
+  end
+
+  defp review_changeset(task, attrs) do
+    task
+    |> Ecto.Changeset.cast(attrs, [:review])
+    |> Ecto.Changeset.validate_required([:review])
+  end
+
+  defp return_path(_scope, "index", _task), do: ~p"/tasks"
+  defp return_path(_scope, "show", task), do: ~p"/tasks/#{task}"
 end
